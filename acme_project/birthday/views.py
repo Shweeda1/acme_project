618bb3b580fed1f54363439894abdacd20c5ae82
Семен Замоздрин <sema686410@gmail.com>
2026-03-27 16:40:57 +0300

    Добавление валидации. Пользовательские изображения

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import Birthday
 
-
-def birthday(request):
-    form = BirthdayForm(request.GET or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(
+        request.POST or None,
+        # Файлы, переданные в запросе, указываются отдельно.
+        files=request.FILES or None,
+        instance=instance
+    )
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        
+            form.cleaned_data['birthday'])
         context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
